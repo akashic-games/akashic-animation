111bfb71d67d936cdb1813391b5d6d3171e717e4
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2021-01-25 18:43:28 +0900

    Add akashic/eslint-config

--- src/aps/index.ts
+++ src/aps/index.ts
@@ -1,6 +1,6 @@
+import { ParticleInitialParameterObject, EmitterParameterObject, EmitterStatus, Emitter } from "./Emitter";
+export { ParticleInitialParameterObject, EmitterParameterObject, EmitterStatus, Emitter };
 import {Particle} from "./Particle";
 export {Particle};
-import {ParticleInitialParameterObject, EmitterParameterObject, EmitterStatus, Emitter} from "./Emitter";
-export {ParticleInitialParameterObject, EmitterParameterObject, EmitterStatus, Emitter};
 import {ParticleSystem} from "./ParticleSystem";
 export {ParticleSystem};
